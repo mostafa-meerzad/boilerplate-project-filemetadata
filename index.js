945b7bb6f59589f50f3ced98c6f7aad8c9c6c83f
mostafa <mostafameerzad@gmail.com>
2024-01-17 14:24:57 +0430

    use multer to get files sent from client-side and save them

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
+const upload = multer({ dest: "uploads/" });
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
